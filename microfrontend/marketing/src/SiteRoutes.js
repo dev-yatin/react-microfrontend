663581c6a872cea7e7376b8b3b235b4496f5ca61
Yatin-Gupta <yatin.gupta1493@gmail.com>
2022-06-18 13:16:37 +0530

    add custom class name generate to prevent jss conflict on production

--- microfrontend/marketing/src/SiteRoutes.js
+++ microfrontend/marketing/src/SiteRoutes.js
@@ -1,21 +1,32 @@
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// To prevent css class names conflict in production when different microforntend deployed
+const generateClassName = createGenerateClassName({
+  productionPrefix: "mar",
+});
+
 const SiteRoutes = () => {
   return (
     <React.Fragment>
-      <Router>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Landing />} />
-          </Route>
-          <Route path="/pricing">
-            <Route index element={<Pricing />} />
-          </Route>
-        </Routes>
-      </Router>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Landing />} />
+            </Route>
+            <Route path="/pricing">
+              <Route index element={<Pricing />} />
+            </Route>
+          </Routes>
+        </Router>
+      </StylesProvider>
     </React.Fragment>
   );
 };
