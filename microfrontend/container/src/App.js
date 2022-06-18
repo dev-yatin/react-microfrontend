fc465a56a8df6d654aa5376fd60001391e90b874
Yatin-Gupta <yatin.gupta1493@gmail.com>
2022-06-18 12:09:58 +0530

    Add Header component

--- microfrontend/container/src/App.js
+++ microfrontend/container/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import Marketing from "./components/Marketing";
 
 const App = (props) => {
   return (
     <React.Fragment>
-      <h1>Hello World</h1>
-      <hr />
+      <Header />
       <Marketing />
     </React.Fragment>
   );
